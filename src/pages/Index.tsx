2779b99805847dacbd4e04438bfe7a1c1cb12dfa
[dyad] <huulong111@gmail.com>
2025-07-28 00:42:32 +0700

    [dyad] Thay đổi hình ảnh thành h5.webp - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import h1 from "@/image/h1.webp";
 import h2 from "@/image/h2.webp";
 import h3 from "@/image/h3.webp";
-import h8 from "@/image/h8.webp";
+import h5 from "@/image/h5.webp";
 import h8_bg from "@/image/h8_bg.webp";
 import { ContactForm } from "@/components/ContactForm";
 import YoutubeEmbed from "@/components/YoutubeEmbed";
@@ -15,7 +15,7 @@ const Index = () => {
           <img src={h2} alt="Hình ảnh 2" className="w-full h-auto block" />
           <img src={h3} alt="Hình ảnh 3" className="w-full h-auto block" />
           <YoutubeEmbed embedId="dQw4w9WgXcQ" />
-          <img src={h8} alt="Hình ảnh 5" className="w-full h-auto block" />
+          <img src={h5} alt="Hình ảnh 5" className="w-full h-auto block" />
           <YoutubeEmbed embedId="L_LUpnjgPso" />
           <img src={h8_bg} alt="Hình ảnh 7" className="w-full h-auto block" />
           <ContactForm />
