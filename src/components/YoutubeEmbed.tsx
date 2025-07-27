edde35ae6ce23c15369cd08f953bfdf2d5028755
[dyad] <huulong111@gmail.com>
2025-07-28 00:48:41 +0700

    [dyad] Điều chỉnh kích thước video dọc - wrote 2 file(s)

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 interface YoutubeEmbedProps {
   embedId: string;
+  isShort?: boolean;
 }
 
-const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId }) => (
-  <div className="relative overflow-hidden pb-[56.25%] h-0 bg-black">
+const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId, isShort = false }) => (
+  <div className={`relative overflow-hidden h-0 bg-black ${isShort ? "pb-[177.78%]" : "pb-[56.25%]"}`}>
     <iframe
       className="absolute top-0 left-0 w-full h-full"
       src={`https://www.youtube.com/embed/${embedId}`}
