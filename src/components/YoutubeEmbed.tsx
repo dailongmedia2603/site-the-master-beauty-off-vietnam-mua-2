938a9737be8c8dec96e47adad97030c5a9d02512
[dyad] <huulong111@gmail.com>
2025-07-28 00:50:39 +0700

    [dyad] Bật tính năng lặp lại video - wrote 1 file(s)

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -9,7 +9,7 @@ const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId, isShort = false })
   <div className={`relative overflow-hidden h-0 bg-black ${isShort ? "pb-[177.78%]" : "pb-[56.25%]"}`}>
     <iframe
       className="absolute top-0 left-0 w-full h-full"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      src={`https://www.youtube.com/embed/${embedId}?loop=1&playlist=${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
